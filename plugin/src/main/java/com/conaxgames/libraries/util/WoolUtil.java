865ec568b711cd231bb34a72228c0cc4c670ec1e
bowpy <57394670+bowpy@users.noreply.github.com>
2021-10-12 16:37:29 +0100

    Changed WoolUtil and deploying 0.8

--- plugin/src/main/java/com/conaxgames/libraries/util/WoolUtil.java
+++ plugin/src/main/java/com/conaxgames/libraries/util/WoolUtil.java
@@ -8,25 +8,6 @@ import java.util.Objects;
 
 public final class WoolUtil {
 
-	private static final ArrayList<ChatColor> woolColors = new ArrayList<>(Arrays.asList(
-			ChatColor.WHITE,
-			ChatColor.GOLD,
-			ChatColor.LIGHT_PURPLE,
-			ChatColor.AQUA,
-			ChatColor.YELLOW,
-			ChatColor.GREEN,
-			ChatColor.LIGHT_PURPLE,
-			ChatColor.DARK_GRAY,
-			ChatColor.GRAY,
-			ChatColor.DARK_AQUA,
-			ChatColor.DARK_PURPLE,
-			ChatColor.BLUE,
-			ChatColor.BLACK,
-			ChatColor.DARK_GREEN,
-			ChatColor.RED,
-			ChatColor.BLACK
-	));
-
 	private static final ArrayList<String> woolCCs = new ArrayList<>(Arrays.asList(
 			CC.WHITE,
 			CC.GOLD,
@@ -47,9 +28,38 @@ public final class WoolUtil {
 	));
 
 	public static int convertChatColorToWoolData(ChatColor color) {
-		if (color == ChatColor.DARK_RED) color = ChatColor.RED;
-
-		return woolColors.indexOf(color);
+		switch (color) {
+			case BLACK:
+				return 15;
+			case DARK_BLUE:
+			case BLUE:
+				return 11;
+			case DARK_GREEN:
+				return 13;
+			case DARK_AQUA:
+				return 9;
+			case DARK_RED:
+			case RED:
+				return 14;
+			case DARK_PURPLE:
+				return 10;
+			case GOLD:
+				return 1;
+			case GRAY:
+				return 8;
+			case DARK_GRAY:
+				return 7;
+			case GREEN:
+				return 5;
+			case AQUA:
+				return 3;
+			case LIGHT_PURPLE:
+				return 2;
+			case YELLOW:
+				return 4;
+			default:
+				return 0;
+		}
 	}
 
 	public static int convertCCToWoolData(String color) {
