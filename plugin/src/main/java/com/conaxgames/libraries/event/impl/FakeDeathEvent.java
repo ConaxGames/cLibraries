a56a983860ab643688c174434e7904d7bcc71c9c
bowpy <57394670+bowpy@users.noreply.github.com>
2021-12-16 20:04:51 +0000

    Added FakeDeathEvent

---
+++ plugin/src/main/java/com/conaxgames/libraries/event/impl/FakeDeathEvent.java
@@ -0,0 +1,31 @@
+package com.conaxgames.libraries.event.impl;
+
+import com.conaxgames.libraries.event.CancellableEvent;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+@Getter
+public class FakeDeathEvent extends CancellableEvent {
+    private final EntityDamageByEntityEvent event;
+    private final Player dead;
+    private final Player killer;
+
+    public FakeDeathEvent(EntityDamageByEntityEvent event, Player dead, Player killer) {
+        this.event = event;
+        this.dead = dead;
+        this.killer = killer;
+    }
+
+    private boolean cancelled;
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.cancelled = b;
+    }
+}
--- plugin/src/main/java/com/conaxgames/libraries/listener/PlayerListener.java
+++ plugin/src/main/java/com/conaxgames/libraries/listener/PlayerListener.java
@@ -2,10 +2,13 @@ package com.conaxgames.libraries.listener;
 
 import com.conaxgames.libraries.LibraryPlugin;
 import com.conaxgames.libraries.board.Board;
+import com.conaxgames.libraries.event.impl.FakeDeathEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -36,6 +39,25 @@ public class PlayerListener implements Listener {
 
         if (LibraryPlugin.getInstance().getBoardManager() != null) {
             LibraryPlugin.getInstance().getBoardManager().getPlayerBoards().remove(player.getUniqueId());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onDamage(EntityDamageByEntityEvent event) {
+        /* If the event entity is not a player, we don't need to continue */
+        if (!(event.getEntity() instanceof Player)) {
+            return;
+        }
+
+        if (event.isCancelled()) {
+            return;
+        }
+
+        Player dead = (Player) event.getEntity();
+        Player damager = (Player) event.getDamager();
+
+        if ((event.getDamage()/2) >= dead.getHealth()) {
+            new FakeDeathEvent(event, dead, damager).call();
         }
     }
 }
