a674c6bd5dab319d25f94df6a1bce4e35c7e702d
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2022-03-27 15:25:51 +0100

    Fixed bad import for 1.18.1 support & Added 1.18.2 support

---
+++ 1_18_R2/src/main/java/com/conaxgames/libraries/nms/v1_18_R2/LibNMSManager.java
@@ -0,0 +1,7 @@
+package com.conaxgames.libraries.nms.v1_18_R2;
+
+public class LibNMSManager extends com.conaxgames.libraries.nms.LibNMSManager {
+
+    public LibNMSManager() {super(new LibNMSManagers());}
+
+}
