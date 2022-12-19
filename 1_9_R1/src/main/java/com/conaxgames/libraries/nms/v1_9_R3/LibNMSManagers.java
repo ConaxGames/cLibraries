a7f8a0158787c200db53a88231e5dcd8ad9f49de
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2022-12-19 19:27:41 +0000

    Added v1_9_R1 support requested by @luver

---
+++ 1_9_R1/src/main/java/com/conaxgames/libraries/nms/v1_9_R3/LibNMSManagers.java
@@ -0,0 +1,10 @@
+package com.conaxgames.libraries.nms.v1_9_R3;
+
+import com.conaxgames.libraries.nms.v1_9_R3.management.utility.UtilityNMSManager;
+
+public class LibNMSManagers extends com.conaxgames.libraries.nms.management.LibNMSManagers {
+
+    public LibNMSManagers() {
+        super(new UtilityNMSManager());
+    }
+}
