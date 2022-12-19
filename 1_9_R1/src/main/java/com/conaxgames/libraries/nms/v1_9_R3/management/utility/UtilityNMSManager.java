ceb5a1851488933788effc5ff6fbb466dd4519df
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2022-12-19 19:27:41 +0000

    Added v1_9_R1 support requested by @luver

---
+++ 1_9_R1/src/main/java/com/conaxgames/libraries/nms/v1_9_R3/management/utility/UtilityNMSManager.java
@@ -0,0 +1,9 @@
+package com.conaxgames.libraries.nms.v1_9_R3.management.utility;
+
+public class UtilityNMSManager extends com.conaxgames.libraries.nms.management.utility.UtilityNMSManager {
+
+    @Override
+    public String translateHex(String message) {
+        return message;
+    }
+}
