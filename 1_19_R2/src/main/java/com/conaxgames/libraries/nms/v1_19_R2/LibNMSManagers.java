83a55765e6fd270eac8524cbc4f8eb8c1169e9bf
bowpy <57394670+oliverbowp@users.noreply.github.com>
2022-12-19 18:32:02 +0000

    1.19.3 (R2) NMS

---
+++ 1_19_R2/src/main/java/com/conaxgames/libraries/nms/v1_19_R2/LibNMSManagers.java
@@ -0,0 +1,10 @@
+package com.conaxgames.libraries.nms.v1_19_R2;
+
+import com.conaxgames.libraries.nms.v1_19_R1.management.utility.UtilityNMSManager;
+
+public class LibNMSManagers extends com.conaxgames.libraries.nms.management.LibNMSManagers {
+
+    public LibNMSManagers() {
+        super(new UtilityNMSManager());
+    }
+}
