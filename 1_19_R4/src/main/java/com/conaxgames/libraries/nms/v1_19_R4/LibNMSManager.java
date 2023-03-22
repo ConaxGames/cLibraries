70865f7d7d2a27fb06571947ee5d8cc4a81c5555
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2023-03-22 11:35:58 +0000

    Added 1.19.4 support

---
+++ 1_19_R4/src/main/java/com/conaxgames/libraries/nms/v1_19_R4/LibNMSManager.java
@@ -0,0 +1,7 @@
+package com.conaxgames.libraries.nms.v1_19_R4;
+
+public class LibNMSManager extends com.conaxgames.libraries.nms.LibNMSManager {
+
+    public LibNMSManager() {super(new LibNMSManagers());}
+
+}
