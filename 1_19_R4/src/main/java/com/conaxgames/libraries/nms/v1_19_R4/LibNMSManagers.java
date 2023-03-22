70b2bc34805eb1b532365aef0676393fc7458a1c
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2023-03-22 13:19:20 +0000

    Fixed some bad reflections

--- 1_19_R4/src/main/java/com/conaxgames/libraries/nms/v1_19_R4/LibNMSManagers.java
+++ 1_19_R4/src/main/java/com/conaxgames/libraries/nms/v1_19_R4/LibNMSManagers.java
@@ -1,6 +1,6 @@
 package com.conaxgames.libraries.nms.v1_19_R4;
 
-import com.conaxgames.libraries.nms.v1_19_R4.management.utility.UtilityNMSManager;
+import com.conaxgames.libraries.nms.v1_19_R2.management.utility.UtilityNMSManager;
 
 public class LibNMSManagers extends com.conaxgames.libraries.nms.management.LibNMSManagers {
 
