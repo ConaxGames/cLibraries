7550d4be03e9cf4868b6e8ef9a30f1e0902209fb
bowpy <57394670+oliverbowp@users.noreply.github.com>
2023-05-25 09:17:50 +0100

    Formatter format amount above trillion

--- plugin/src/main/java/com/conaxgames/libraries/util/Formatter.java
+++ plugin/src/main/java/com/conaxgames/libraries/util/Formatter.java
@@ -19,28 +19,33 @@ public class Formatter {
         return String.format("%.2f", number);
     }
 
-    public static String formatMoneyKMBT(long value) {
+    public static String formatMoneyKMBT(double value) {
         //Long.MIN_VALUE == -Long.MIN_VALUE so we need an adjustment here
-        if (value == Long.MIN_VALUE) return formatMoneyKMBT(Long.MIN_VALUE + 1);
+        if (value == Double.MIN_VALUE) return formatMoneyKMBT(Double.MIN_VALUE + 1);
         if (value < 0) return "-" + formatMoneyKMBT(-value);
-        if (value < 1000) return Long.toString(value); //deal with easy case
+        if (value < 1000) return Integer.toString((int)value); //deal with easy case
 
-        Map.Entry<Long, String> e = suffixes.floorEntry(value);
-        Long divideBy = e.getKey();
+        Map.Entry<Double, String> e = suffixes.floorEntry(value);
+        Double divideBy = e.getKey();
         String suffix = e.getValue();
 
-        long truncated = value / (divideBy / 10); //the number part of the output times 10
+        double truncated = value / (divideBy / 10); //the number part of the output times 10
         boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
-        return hasDecimal ? (truncated / 10d) + suffix : (truncated / 10) + suffix;
+        return hasDecimal ? String.format("%.1f", (truncated / 10d)) + suffix : String.format("%.0f", (truncated / 10)) + suffix;
     }
 
-    private static final NavigableMap<Long, String> suffixes = new TreeMap<>();
+    private static final NavigableMap<Double, String> suffixes = new TreeMap<>();
     static {
-        suffixes.put(1_000L, "k");
-        suffixes.put(1_000_000L, "M");
-        suffixes.put(1_000_000_000L, "G");
-        suffixes.put(1_000_000_000_000L, "T");
-        suffixes.put(1_000_000_000_000_000L, "P");
-        suffixes.put(1_000_000_000_000_000_000L, "E");
+        suffixes.put(1_000D, "k");
+        suffixes.put(1_000_000D, "M");
+        suffixes.put(1_000_000_000D, "B");
+        suffixes.put(1_000_000_000_000D, "T");
+        suffixes.put(1_000_000_000_000_000D, "QD");
+        suffixes.put(1_000_000_000_000_000_000D, "QT");
+        suffixes.put(1_000_000_000_000_000_000_000D, "SX");
+        suffixes.put(1_000_000_000_000_000_000_000_000D, "ST");
+        suffixes.put(1_000_000_000_000_000_000_000_000_000D, "O");
+        suffixes.put(1_000_000_000_000_000_000_000_000_000_000D, "N");
+        suffixes.put(1_000_000_000_000_000_000_000_000_000_000_000D, "D");
     }
 }
