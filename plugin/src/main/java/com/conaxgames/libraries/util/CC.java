a16cbbe881b420364b406e828016d78f792a973e
bowpy <57394670+oliverbowp@users.noreply.github.com>
2023-06-07 18:07:20 +0100

    Fixed bold CC

--- plugin/src/main/java/com/conaxgames/libraries/util/CC.java
+++ plugin/src/main/java/com/conaxgames/libraries/util/CC.java
@@ -10,8 +10,8 @@ public final class CC {
 
 	public static String PRIMARY = ChatColor.YELLOW.toString();
 	public static String SECONDARY = ChatColor.GOLD.toString();
-	public static String B_PRIMARY = ChatColor.YELLOW.toString() + ChatColor.BOLD.toString();
-	public static String B_SECONDARY = ChatColor.GOLD.toString() + ChatColor.BOLD.toString();
+	public static String B_PRIMARY = ChatColor.YELLOW + ChatColor.BOLD.toString();
+	public static String B_SECONDARY = ChatColor.GOLD + ChatColor.BOLD.toString();
 
 	public static final String U = ChatColor.UNDERLINE.toString();
 	public static final String BLUE = ChatColor.BLUE.toString();
@@ -48,26 +48,32 @@ public final class CC {
 	public static final String D_PURPLE = DARK_PURPLE;
 	public static final String D_RED = DARK_RED;
 	public static final String LIGHT_PURPLE = ChatColor.LIGHT_PURPLE.toString();
-	public static final String B_LIGHT_PURPLE = CC.B + CC.LIGHT_PURPLE;
 	public static final String L_PURPLE = LIGHT_PURPLE;
 	public static final String PINK = L_PURPLE;
-	public static final String B_BLUE = BLUE + B;
-	public static final String B_AQUA = AQUA + B;
-	public static final String B_YELLOW = YELLOW + B;
-	public static final String B_RED = RED + B;
-	public static final String B_GRAY = GRAY + B;
-	public static final String B_GOLD = GOLD + B;
-	public static final String B_GREEN = GREEN + B;
-	public static final String B_WHITE = WHITE + B;
-	public static final String B_BLACK = BLACK + B;
-	public static final String BD_BLUE = D_BLUE + B;
-	public static final String BD_AQUA = D_AQUA + B;
-	public static final String BD_DARKAQUA = D_AQUA + B;
-	public static final String BD_GRAY = D_GRAY + B;
-	public static final String BD_GREEN = D_GREEN + B;
-	public static final String BD_PURPLE = D_PURPLE + B;
-	public static final String BD_RED = D_RED + B;
-	public static final String BL_PURPLE = L_PURPLE + B;
+
+	public static final String B_LIGHT_PURPLE =  CC.LIGHT_PURPLE + CC.B;
+	public static final String B_BLUE =  CC.BLUE + CC.B;
+	public static final String B_AQUA =  CC.AQUA + CC.B;
+	public static final String B_YELLOW =  CC.YELLOW + CC.B;
+	public static final String B_RED =  CC.RED + CC.B ;
+	public static final String B_GRAY =  CC.GRAY + CC.B;
+	public static final String B_GOLD =  CC.GOLD + CC.B;
+	public static final String B_GREEN =  CC.GREEN + CC.B;
+	public static final String B_WHITE =  CC.WHITE + CC.B;
+	public static final String B_BLACK =  CC.BLACK + CC.B;
+	public static final String BD_BLUE =  CC.D_BLUE + CC.B;
+	public static final String BD_AQUA =  CC.D_AQUA + CC.B;
+	public static final String BD_DARKAQUA =  CC.D_AQUA + CC.B;
+	public static final String BD_GRAY =  CC.D_GRAY + CC.B;
+	public static final String BD_GREEN =  CC.D_GREEN + CC.B;
+	public static final String BD_PURPLE =  CC.D_PURPLE + CC.B;
+	public static final String BD_RED =  CC.D_RED + CC.B;
+	public static final String BL_PURPLE =  CC.L_PURPLE + CC.B;
+	public static final String B_DARK_RED =  CC.DARK_RED + CC.B;
+	public static final String B_DARK_GREEN =  CC.DARK_GREEN + CC.B;
+	public static final String B_DARK_PURPLE =  CC.DARK_PURPLE + CC.B;
+	public static final String B_DARK_AQUA =  CC.DARK_AQUA + CC.B;
+
 	public static final String I_BLUE = BLUE + I;
 	public static final String I_AQUA = AQUA + I;
 	public static final String I_YELLOW = YELLOW + I;
@@ -90,15 +96,11 @@ public final class CC {
 	public static final String U_GREEN = ChatColor.UNDERLINE + GREEN;
 	public static final String U_GRAY = ChatColor.UNDERLINE + GRAY;
 	public static final String U_WHITE = ChatColor.UNDERLINE + WHITE;
-	public static final String B_DARK_RED = ChatColor.DARK_RED.toString() + ChatColor.BOLD;
-	public static final String B_DARK_GREEN = CC.B + CC.DARK_GREEN;
-	public static final String B_DARK_PURPLE = CC.B + CC.DARK_PURPLE;
 	public static final String U_GOLD = CC.U + CC.GOLD;
 	public static final String U_YELLOW = CC.U + CC.YELLOW;
 	public static final String U_LIGHT_PURPLE = CC.U + CC.LIGHT_PURPLE;
 	public static final String U_AQUA = CC.U + CC.AQUA;
 	public static final String U_DARK_AQUA = CC.U + CC.DARK_AQUA;
-	public static final String B_DARK_AQUA = CC.B + CC.DARK_AQUA;
 
 	public static String translate(String string) {
 		String translatedHex = LibNMSManager.getInstance().getManagers().getUtilityNMSManager().translateHex(string);
@@ -115,8 +117,8 @@ public final class CC {
 
 		PRIMARY = primary;
 		SECONDARY = secondary;
-		B_PRIMARY = primary + ChatColor.BOLD.toString();
-		B_SECONDARY = secondary + ChatColor.BOLD.toString();
+		B_PRIMARY = primary + ChatColor.BOLD;
+		B_SECONDARY = secondary + ChatColor.BOLD;
 	}
 
 }
