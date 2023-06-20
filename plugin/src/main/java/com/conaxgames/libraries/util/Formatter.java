d02d8b3b1a2d15e81ab930d23864ab3d69c92e61
bowpy <57394670+oliverbowp@users.noreply.github.com>
2023-06-20 19:38:21 +0100

    HH:MM:SS time formatter

--- plugin/src/main/java/com/conaxgames/libraries/util/Formatter.java
+++ plugin/src/main/java/com/conaxgames/libraries/util/Formatter.java
@@ -4,6 +4,7 @@ import java.text.NumberFormat;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
 
 public class Formatter {
 
@@ -32,6 +33,31 @@ public class Formatter {
         double truncated = value / (divideBy / 10); //the number part of the output times 10
         boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
         return hasDecimal ? String.format("%.1f", (truncated / 10d)) + suffix : String.format("%.0f", (truncated / 10)) + suffix;
+    }
+
+    public static String formatTimeMMSS(long secs) {
+        return formatTimeMMSS((int) TimeUnit.MILLISECONDS.toSeconds(secs));
+    }
+
+    /**
+     * Formats the time into a format of HH:MM:SS. Example: 3600 (1 hour) displays as '01:00:00'
+     *
+     * @param secs The input time, in seconds.
+     * @return The HH:MM:SS formatted time.
+     */
+    public static String formatTimeMMSS(int secs) {
+        // Calculate the seconds to display:
+        int seconds = secs % 60;
+        secs -= seconds;
+
+        // Calculate the minutes:
+        long minutesCount = secs / 60;
+        long minutes = minutesCount % 60;
+        minutesCount -= minutes;
+
+        long hours = minutesCount / 60;
+
+        return (hours > 0 ? (hours < 10 ? "0" : "") + hours + ":" : "") + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
     }
 
     private static final NavigableMap<Double, String> suffixes = new TreeMap<>();
