0493c27461428ee9165dd6ff2edf0e8985dc777a
bowpy <57394670+oliverbowp@users.noreply.github.com>
2023-06-27 20:56:48 +0100

    1_20_R1

--- plugin/src/main/java/com/conaxgames/libraries/nms/LibServerVersion.java
+++ plugin/src/main/java/com/conaxgames/libraries/nms/LibServerVersion.java
@@ -11,7 +11,8 @@ public enum LibServerVersion {
     v1_19_R1,
     v1_19_R2,
     v1_19_R3,
-    v1_19_R4;
+    v1_19_R4,
+    v1_20_R1;
 
     public boolean after(LibServerVersion serverVersion) {
         return this.ordinal() > serverVersion.ordinal();
