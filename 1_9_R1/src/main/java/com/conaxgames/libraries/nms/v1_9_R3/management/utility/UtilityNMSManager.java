c55a81ded56466d61d5b532f037f052383a89dc0
bowpy <57394670+oliverbowp@users.noreply.github.com>
2023-07-07 19:07:31 +0100

    ItemMeta setUnbreakable NMS

--- 1_9_R1/src/main/java/com/conaxgames/libraries/nms/v1_9_R3/management/utility/UtilityNMSManager.java
+++ 1_9_R1/src/main/java/com/conaxgames/libraries/nms/v1_9_R3/management/utility/UtilityNMSManager.java
@@ -1,9 +1,17 @@
 package com.conaxgames.libraries.nms.v1_9_R3.management.utility;
 
+import org.bukkit.inventory.meta.ItemMeta;
+
 public class UtilityNMSManager extends com.conaxgames.libraries.nms.management.utility.UtilityNMSManager {
 
     @Override
     public String translateHex(String message) {
         return message;
+    }
+
+    @Override
+    public ItemMeta setUnbreakable(ItemMeta meta, boolean value) {
+        meta.spigot().setUnbreakable(value);
+        return meta;
     }
 }
--- plugin/src/main/java/com/conaxgames/libraries/nms/management/utility/UtilityNMSManager.java
+++ plugin/src/main/java/com/conaxgames/libraries/nms/management/utility/UtilityNMSManager.java
@@ -1,6 +1,10 @@
 package com.conaxgames.libraries.nms.management.utility;
 
+import org.bukkit.inventory.meta.ItemMeta;
+
 public abstract class UtilityNMSManager {
 
     public abstract String translateHex(String message);
+
+    public abstract ItemMeta setUnbreakable(ItemMeta meta, boolean value);
 }
