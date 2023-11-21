9a2e59b9aba95560c7d878ea193c4529601fa4bb
Brendan Callanan <brendan.business@outlook.com>
2023-11-21 18:46:09 +0100

    Added support for both #0000 and &#0000 hex color codes

--- plugin/src/main/java/com/conaxgames/libraries/util/CC.java
+++ plugin/src/main/java/com/conaxgames/libraries/util/CC.java
@@ -111,13 +111,13 @@ public final class CC {
 	}
 
 	private static String translateHex(String message) {
-		Pattern hexPattern = Pattern.compile("&#[A-Fa-f0-9]{6}");
-
+		Pattern hexPattern = Pattern.compile("#[A-Fa-f0-9]{6}|&#[A-Fa-f0-9]{6}");
 		// Handle hexadecimal color codes
 		Matcher hexMatcher = hexPattern.matcher(message);
 		while (hexMatcher.find()) {
 			String hexColor = hexMatcher.group();
-			message = message.replace(hexColor, net.md_5.bungee.api.ChatColor.of(hexColor).toString());
+			hexColor = hexColor.replace("#", "").replace("&#", "");
+			message = message.replace(hexMatcher.group(), net.md_5.bungee.api.ChatColor.of(hexColor).toString());
 		}
 
 		return message; // Your default implementation (1.8 NMS behavior)
