b289a0a238e32a850a2265fc6b28fe544c6a27e3
bowpy <57394670+oliverbowp@users.noreply.github.com>
2023-11-21 09:38:53 +0000

    Tristate enum

---
+++ plugin/src/main/java/com/conaxgames/libraries/util/Tristate.java
@@ -0,0 +1,53 @@
+package com.conaxgames.libraries.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BooleanSupplier;
+
+public enum Tristate {
+    NOT_SET,
+    FALSE,
+    TRUE;
+
+    public @Nullable Boolean toBoolean() {
+        switch (this) {
+            case TRUE:
+                return Boolean.TRUE;
+            case FALSE:
+                return Boolean.FALSE;
+            default:
+                return null;
+        }
+    }
+
+    public boolean toBooleanOrDefault(final boolean def) {
+        switch (this) {
+            case TRUE:
+                return true;
+            case FALSE:
+                return false;
+            default:
+                return def;
+        }
+    }
+
+    public boolean toBooleanOrElseGet(final @NotNull BooleanSupplier supplier) {
+        switch (this) {
+            case TRUE:
+                return true;
+            case FALSE:
+                return false;
+            default:
+                return supplier.getAsBoolean();
+        }
+    }
+
+    public static @NotNull Tristate fromBoolean(final boolean value) {
+        return value ? TRUE : FALSE;
+    }
+
+    public static @NotNull Tristate fromBoolean(final @Nullable Boolean value) {
+        return value == null ? NOT_SET : fromBoolean(value);
+    }
+}
