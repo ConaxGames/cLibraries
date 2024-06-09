1dd04e18e822903b915cb932f8052a41cfc091d6
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2024-06-09 09:07:33 +0100

    Added v1_20_R4 support

--- plugin/src/main/java/com/conaxgames/libraries/nms/LibServerVersion.java
+++ plugin/src/main/java/com/conaxgames/libraries/nms/LibServerVersion.java
@@ -15,7 +15,8 @@ public enum LibServerVersion {
     v1_19_R4,
     v1_20_R1,
     v1_20_R2,
-    v1_20_R3;
+    v1_20_R3,
+    v1_20_R4;
 
     public boolean after(LibServerVersion serverVersion) {
         return this.ordinal() > serverVersion.ordinal();
