80ed0f0a16b0923304a663ebda3b8d71538b26e2
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2024-06-09 12:20:30 +0100

    Removed redundant todo tasks

--- plugin/src/main/java/com/conaxgames/libraries/util/ClassUtils.java
+++ plugin/src/main/java/com/conaxgames/libraries/util/ClassUtils.java
@@ -39,7 +39,6 @@ public final class ClassUtils {
      * @param packageName The package to scan classes in.
      * @return The classes in the package packageName.
      */
-    //TODO: Make this not require a Plugin object.
     public static Collection<Class<?>> getClassesInPackage(Plugin plugin, String packageName) {
         Collection<Class<?>> classes = new ArrayList<>();
 
