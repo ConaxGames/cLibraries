0e3793a27e59b89bb59a4495fffd40f41e8bb127
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2024-06-21 11:06:34 +0100

    Added `v1_21_R1` nms

--- plugin/src/main/java/com/conaxgames/libraries/nms/LibServerVersion.java
+++ plugin/src/main/java/com/conaxgames/libraries/nms/LibServerVersion.java
@@ -16,7 +16,8 @@ public enum LibServerVersion {
     v1_20_R1,
     v1_20_R2,
     v1_20_R3,
-    v1_20_R4;
+    v1_20_R4,
+    v1_21_R1;
 
     public boolean after(LibServerVersion serverVersion) {
         return this.ordinal() > serverVersion.ordinal();
