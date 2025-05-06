63e123b0c01d84029c7c51f5fcdcab1d95e89378
Brendan Callanan <brendan.business@outlook.com>
2025-05-06 21:32:40 +0100

    refactor: improve LibraryPlugin organization and readability
    - Organize fields and methods in a logical way
    - Fix redundant getters and setters
    - Improve documentation
    - Make the initialization flow clearer

--- src/main/java/com/conaxgames/libraries/LibraryPlugin.java
+++ src/main/java/com/conaxgames/libraries/LibraryPlugin.java
@@ -11,28 +11,28 @@ import com.conaxgames.libraries.module.ModuleManager;
 import com.conaxgames.libraries.timer.TimerManager;
 import com.google.common.base.Joiner;
 import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.IllegalPluginAccessException;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
 
+/**
+ * Core library plugin class that manages the initialization and lifecycles
+ * of all library components.
+ */
 @Getter
-@Setter
 public class LibraryPlugin {
 
-    @Getter @Setter
+    // Singleton instance
     private static LibraryPlugin instance;
-
-    /*
-     * The plugin can only be setup once due to managers and listeners being registered.
-     * This can be called
-     */
-    private boolean setup;
-
+    
+    // Core properties
     private JavaPlugin plugin;
-
+    private boolean setup;
+    private Settings settings;
+    
+    // Managers
     private LibraryLogger libraryLogger;
     private TimerManager timerManager;
     private PaperCommandManager paperCommandManager;
@@ -40,8 +40,13 @@ public class LibraryPlugin {
     private BoardManager boardManager;
     private HookManager hookManager;
     private ModuleManager moduleManager;
-    private Settings settings = null;
 
+    /**
+     * Gets the singleton instance of the LibraryPlugin.
+     * 
+     * @return The LibraryPlugin instance
+     * @throws IllegalPluginAccessException if the library is not registered
+     */
     public static LibraryPlugin getInstance() {
         if (instance == null) {
             throw new IllegalPluginAccessException("cLibraries is not registered.");
@@ -49,23 +54,77 @@ public class LibraryPlugin {
         return instance;
     }
 
-    public LibraryPlugin onEnable(JavaPlugin plugin, String debugPrimary, String debugSecondary, String moduleCommandAlias, String moduleCommandPerm) {
+    /**
+     * Initializes the LibraryPlugin with the provided plugin instance and configuration.
+     * This should only be called once during plugin initialization.
+     *
+     * @param plugin The JavaPlugin instance
+     * @param debugPrimary The primary debug configuration
+     * @param debugSecondary The secondary debug configuration
+     * @param moduleCommandAlias The command alias for module commands
+     * @param moduleCommandPerm The permission for module commands
+     * @return This LibraryPlugin instance for chaining
+     */
+    public LibraryPlugin onEnable(JavaPlugin plugin, String debugPrimary, String debugSecondary, 
+                                 String moduleCommandAlias, String moduleCommandPerm) {
+        // Prevent multiple initialization
         if (this.setup) {
-            String authors = Joiner.on(", ").join(this.plugin.getDescription().getAuthors());
-            libraryLogger.toConsole(
-                    "cLibraries",
-                    Arrays.asList(
-                            " ",
-                            "cLibraries is already setup!", this.plugin.getName() + " has called onEnable twice!",
-                            "Please nag " + authors + " to fix this!",
-                            " ")
-            );
+            logMultipleInitializationWarning();
             return this;
         }
 
+        // Set instance and plugin
         instance = this;
         this.plugin = plugin;
 
+        // Initialize settings
+        initializeSettings();
+        
+        // Initialize core components
+        this.libraryLogger = new LibraryLogger(plugin, debugPrimary, debugSecondary);
+        this.paperCommandManager = new PaperCommandManager(this.plugin);
+        this.commandRegistry = new CommandRegistry(this, paperCommandManager);
+        this.hookManager = new HookManager(this);
+        this.timerManager = new TimerManager();
+        this.moduleManager = new ModuleManager(this, moduleCommandAlias, moduleCommandPerm);
+
+        // Register event listeners
+        registerEventListeners();
+
+        // Complete initialization
+        new LibraryPluginEnableEvent().call();
+        this.setup = true;
+        logSuccessfulInitialization();
+        
+        return this;
+    }
+
+    /**
+     * Disables the plugin and all modules.
+     * 
+     * @return This LibraryPlugin instance for chaining
+     */
+    public LibraryPlugin onDisable() {
+        this.moduleManager.disableAllModules();
+        return this;
+    }
+
+    /**
+     * Sets the board manager and schedules its task.
+     * 
+     * @param boardManager The board manager instance
+     */
+    public void setBoardManager(BoardManager boardManager) {
+        this.boardManager = boardManager;
+        long interval = this.boardManager.getAdapter().getInterval();
+        this.plugin.getServer().getScheduler().runTaskTimerAsynchronously(
+            this.plugin, this.boardManager, 0L, interval
+        );
+    }
+
+    // Private helper methods
+
+    private void initializeSettings() {
         try {
             settings = new Settings();
         } catch (Exception e) {
@@ -73,51 +132,39 @@ public class LibraryPlugin {
             Bukkit.getLogger().info("cLibraries settings were unable to load!");
             Bukkit.getLogger().info(" ");
         }
+    }
 
-        this.libraryLogger = new LibraryLogger(plugin, debugPrimary, debugSecondary);
-
-        this.paperCommandManager = new PaperCommandManager(this.plugin);
-        this.commandRegistry = new CommandRegistry(this, paperCommandManager);
-
-        this.hookManager = new HookManager(this);
-        this.timerManager = new TimerManager();
-        this.moduleManager = new ModuleManager(this, moduleCommandAlias, moduleCommandPerm);
-
+    private void registerEventListeners() {
         Arrays.asList(
-                new PlayerListener(this),
-                this.hookManager
-        ).forEach(l -> Bukkit.getPluginManager().registerEvents(l, this.plugin));
-
-        new LibraryPluginEnableEvent().call();
-        this.setup = true;
-
-        String authors = Joiner.on(", ").join(this.plugin.getDescription().getAuthors());
-        this.libraryLogger.toConsole("cLibraries",
-                Arrays.asList(
-                        " ",
-                        "cLibraries instance has been setup for " + this.plugin.getName() + " (" + authors + ")",
-                        " ")
+            new PlayerListener(this),
+            this.hookManager
+        ).forEach(listener -> 
+            Bukkit.getPluginManager().registerEvents(listener, this.plugin)
         );
-        return this;
     }
 
-    public LibraryPlugin onDisable() {
-        this.moduleManager.disableAllModules();
-        return this;
+    private void logMultipleInitializationWarning() {
+        String authors = Joiner.on(", ").join(this.plugin.getDescription().getAuthors());
+        libraryLogger.toConsole(
+            "cLibraries",
+            Arrays.asList(
+                " ",
+                "cLibraries is already setup!", this.plugin.getName() + " has called onEnable twice!",
+                "Please nag " + authors + " to fix this!",
+                " "
+            )
+        );
     }
 
-    public Settings getSettings() {
-        return settings;
+    private void logSuccessfulInitialization() {
+        String authors = Joiner.on(", ").join(this.plugin.getDescription().getAuthors());
+        this.libraryLogger.toConsole(
+            "cLibraries",
+            Arrays.asList(
+                " ",
+                "cLibraries instance has been setup for " + this.plugin.getName() + " (" + authors + ")",
+                " "
+            )
+        );
     }
-
-    public JavaPlugin getPlugin() {
-        return plugin;
-    }
-
-    public void setBoardManager(BoardManager boardManager) {
-        this.boardManager = boardManager;
-        long interval = this.boardManager.getAdapter().getInterval();
-        this.plugin.getServer().getScheduler().runTaskTimerAsynchronously(this.plugin, this.boardManager, 0L, interval);
-    }
-
 }
