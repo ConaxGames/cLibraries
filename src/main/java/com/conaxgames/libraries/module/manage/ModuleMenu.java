1b8b00b13f7401c0e3403fcd313ed25bf4e0edfd
Brendan Callanan <brendan.business@outlook.com>
2025-11-01 19:16:10 +0000

    Revert "fix: re-aligning module list menu"
    This reverts commit 3b035e9a1e746a8895cdbaa0b3619f6acaa686b0.

--- src/main/java/com/conaxgames/libraries/module/manage/ModuleMenu.java
+++ src/main/java/com/conaxgames/libraries/module/manage/ModuleMenu.java
@@ -1,7 +1,6 @@
 package com.conaxgames.libraries.module.manage;
 
 import com.conaxgames.libraries.menu.Button;
-import com.conaxgames.libraries.menu.buttons.CloseButton;
 import com.conaxgames.libraries.menu.pagination.PaginatedMenu;
 import com.conaxgames.libraries.module.ModuleManager;
 import org.bukkit.entity.Player;
@@ -16,7 +15,6 @@ public class ModuleMenu extends PaginatedMenu {
 
     public ModuleMenu(ModuleManager moduleManager) {
         this.moduleManager = moduleManager;
-        this.setReservedRows(5);
     }
 
     @Override
@@ -36,20 +34,13 @@ public class ModuleMenu extends PaginatedMenu {
     }
 
     @Override
-    public Map<Integer, Button> getGlobalButtons(Player player) {
-        Map<Integer, Button> buttons = new HashMap<>();
-        buttons.put(49, new CloseButton());
-        return buttons;
-    }
-
-    @Override
     public int previousPageSlot(Player var1) {
-        return 48;
+        return 0;
     }
 
     @Override
     public int nextPageSlot(Player var1) {
-        return 50;
+        return 8;
     }
 
 }
