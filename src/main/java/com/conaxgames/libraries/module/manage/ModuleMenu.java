16300bbadae295ab044d97ff5452518e1fe60877
bennycallanan <brendan.finance@outlook.com>
2026-02-15 16:03:02 +0000

    qol: new module menu layout

--- src/main/java/com/conaxgames/libraries/module/manage/ModuleMenu.java
+++ src/main/java/com/conaxgames/libraries/module/manage/ModuleMenu.java
@@ -4,13 +4,18 @@ import com.conaxgames.libraries.menu.Button;
 import com.conaxgames.libraries.menu.pagination.PaginatedMenu;
 import com.conaxgames.libraries.module.ModuleManager;
 import com.conaxgames.libraries.module.type.Module;
+import com.conaxgames.libraries.util.CC;
+import com.cryptomorin.xseries.XMaterial;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ModuleMenu extends PaginatedMenu {
-
+    
     private final ModuleManager moduleManager;
 
     public ModuleMenu(ModuleManager moduleManager) {
@@ -34,12 +39,47 @@ public class ModuleMenu extends PaginatedMenu {
     }
 
     @Override
+    public Map<Integer, Button> getGlobalButtons(Player player) {
+        Map<Integer, Button> buttons = new HashMap<>();
+        int total = moduleManager.getModules().size();
+        long enabled = moduleManager.getModules().values().stream()
+                .filter(moduleManager::isModuleEnabled)
+                .count();
+        int disabled = (int) (total - enabled);
+
+        Button infoButton = new Button() {
+            @Override
+            public String getName(Player player) {
+                return CC.GOLD + "Module Statistics";
+            }
+
+            @Override
+            public List<String> getDescription(Player player) {
+                List<String> lore = new ArrayList<>();
+                lore.add("");
+                lore.add(CC.GRAY + "Total Modules: " + CC.WHITE + total);
+                lore.add(CC.GRAY + "Enabled: " + CC.GREEN + enabled);
+                lore.add(CC.GRAY + "Disabled: " + CC.RED + disabled);
+                lore.add("");
+                return lore;
+            }
+
+            @Override
+            public Material getMaterial(Player player) {
+                return XMaterial.BOOK.get();
+            }
+        };
+        buttons.put(49, infoButton);
+        return buttons;
+    }
+
+    @Override
     public int previousPageSlot(Player player) {
-        return 45;
+        return 48;
     }
 
     @Override
     public int nextPageSlot(Player player) {
-        return 53;
+        return 50;
     }
 }
