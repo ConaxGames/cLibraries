d74f912102487529c579fbc39c4e4a98a9366046
bennycallanan <brendan.finance@outlook.com>
2026-02-16 11:23:29 +0000

    eol: FakeDeathEvent

--- src/main/java/com/conaxgames/libraries/listener/PlayerListener.java
+++ src/main/java/com/conaxgames/libraries/listener/PlayerListener.java
@@ -1,13 +1,11 @@
 package com.conaxgames.libraries.listener;
 
 import com.conaxgames.libraries.LibraryPlugin;
-import com.conaxgames.libraries.event.impl.FakeDeathEvent;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -38,19 +36,6 @@ public class PlayerListener implements Listener {
         Player player = event.getPlayer();
         if (library.getBoardManager() != null && !player.hasMetadata("cElement")) {
             library.getBoardManager().removeBoard(player);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGH)
-    public void onDamage(EntityDamageByEntityEvent event) {
-        if (!(event.getEntity() instanceof Player)) {
-            return;
-        }
-
-        Player dead = (Player) event.getEntity();
-
-        if ((event.getDamage()/2) >= dead.getHealth()) {
-            new FakeDeathEvent(event, dead, event.getDamager()).call();
         }
     }
 }
