7bb3b01787b71605bb6f26be4b5de08b3e41736f
bennycallanan <brendan.finance@outlook.com>
2026-02-16 11:25:34 +0000

    chore(notes): LibraryPluginEnableEvent

--- src/main/java/com/conaxgames/libraries/event/impl/LibraryPluginEnableEvent.java
+++ src/main/java/com/conaxgames/libraries/event/impl/LibraryPluginEnableEvent.java
@@ -2,6 +2,7 @@ package com.conaxgames.libraries.event.impl;
 
 import com.conaxgames.libraries.event.CancellableEvent;
 
+// TODO: Look further into this event when working on the hook codebase
 public class LibraryPluginEnableEvent extends CancellableEvent {
 
 }
