380efcb31e2bbc6a476a57eef5b33e0350b66708
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2026-09-10 12:18:45 +0100

    fix: render board on create instead of next update

--- src/main/java/com/conaxgames/libraries/board/BoardManager.java
+++ src/main/java/com/conaxgames/libraries/board/BoardManager.java
@@ -40,106 +40,109 @@ public final class BoardManager implements Runnable {
             if (player == null) {
                 return true;
             }
-            try {
-                var board = entry.getValue();
-                var lines = this.lines.apply(player);
-                if (lines.size() > Board.MAX_LINES) {
-                    lines = lines.subList(0, Board.MAX_LINES);
+            update(player, entry.getValue());
+            return false;
+        });
+    }
+
+    private void update(Player player, Board board) {
+        try {
+            var lines = this.lines.apply(player);
+            if (lines.size() > Board.MAX_LINES) {
+                lines = lines.subList(0, Board.MAX_LINES);
+            }
+
+            var translatedTitle = CC.translate(title.apply(player));
+            if (translatedTitle.length() > Board.TITLE_MAX) {
+                translatedTitle = translatedTitle.substring(0, Board.TITLE_MAX);
+            }
+            if (!translatedTitle.equals(board.lastTitle)) {
+                board.lastTitle = translatedTitle;
+                if (Board.MODERN) {
+                    var component = CC.LEGACY.deserialize(translatedTitle);
+                    board.objective.displayName(Board.TEXT_SHADOW
+                            ? component.shadowColor(ShadowColor.shadowColor(0xFF000000))
+                            : component);
+                } else {
+                    board.objective.setDisplayName(translatedTitle);
+                }
+            }
+
+            var entries = board.entries;
+            while (entries.size() > lines.size()) {
+                var removed = entries.removeLast();
+                board.scoreboard.resetScores(removed.key);
+                if (removed.team != null) {
+                    removed.team.unregister();
+                }
+            }
+
+            int i = 0;
+            for (var line : lines.reversed()) {
+                BoardEntry boardEntry;
+                if (i < entries.size()) {
+                    boardEntry = entries.get(i);
+                } else {
+                    boardEntry = new BoardEntry();
+                    boardEntry.key = Board.MODERN ? Integer.toString(i) : Board.ENTRY_KEYS[i];
+                    if (!Board.MODERN) {
+                        boardEntry.team = board.scoreboard.registerNewTeam("board_" + i);
+                        boardEntry.team.addEntry(boardEntry.key);
+                    }
+                    entries.add(boardEntry);
                 }
 
-                var translatedTitle = CC.translate(title.apply(player));
-                if (translatedTitle.length() > Board.TITLE_MAX) {
-                    translatedTitle = translatedTitle.substring(0, Board.TITLE_MAX);
+                var score = board.objective.getScore(boardEntry.key);
+                if (score.getScore() != i + 1) {
+                    score.setScore(i + 1);
                 }
-                if (!translatedTitle.equals(board.lastTitle)) {
-                    board.lastTitle = translatedTitle;
+                if (!line.equals(boardEntry.lastSent)) {
+                    boardEntry.lastSent = line;
+                    var translated = CC.translate(line);
                     if (Board.MODERN) {
-                        var component = CC.LEGACY.deserialize(translatedTitle);
-                        board.objective.displayName(Board.TEXT_SHADOW
+                        var component = CC.LEGACY.deserialize(translated);
+                        score.customName(Board.TEXT_SHADOW
                                 ? component.shadowColor(ShadowColor.shadowColor(0xFF000000))
                                 : component);
                     } else {
-                        board.objective.setDisplayName(translatedTitle);
-                    }
-                }
-
-                var entries = board.entries;
-                while (entries.size() > lines.size()) {
-                    var removed = entries.removeLast();
-                    board.scoreboard.resetScores(removed.key);
-                    if (removed.team != null) {
-                        removed.team.unregister();
-                    }
-                }
-
-                int i = 0;
-                for (var line : lines.reversed()) {
-                    BoardEntry boardEntry;
-                    if (i < entries.size()) {
-                        boardEntry = entries.get(i);
-                    } else {
-                        boardEntry = new BoardEntry();
-                        boardEntry.key = Board.MODERN ? Integer.toString(i) : Board.ENTRY_KEYS[i];
-                        if (!Board.MODERN) {
-                            boardEntry.team = board.scoreboard.registerNewTeam("board_" + i);
-                            boardEntry.team.addEntry(boardEntry.key);
-                        }
-                        entries.add(boardEntry);
-                    }
-
-                    var score = board.objective.getScore(boardEntry.key);
-                    if (score.getScore() != i + 1) {
-                        score.setScore(i + 1);
-                    }
-                    if (!line.equals(boardEntry.lastSent)) {
-                        boardEntry.lastSent = line;
-                        var translated = CC.translate(line);
-                        if (Board.MODERN) {
-                            var component = CC.LEGACY.deserialize(translated);
-                            score.customName(Board.TEXT_SHADOW
-                                    ? component.shadowColor(ShadowColor.shadowColor(0xFF000000))
-                                    : component);
+                        int max = Board.SEGMENT_MAX;
+                        String prefix;
+                        String suffix;
+                        if (translated.length() <= max) {
+                            prefix = translated;
+                            suffix = "";
                         } else {
-                            int max = Board.SEGMENT_MAX;
-                            String prefix;
-                            String suffix;
-                            if (translated.length() <= max) {
-                                prefix = translated;
-                                suffix = "";
+                            prefix = translated.substring(0, max);
+                            int lastColor = prefix.lastIndexOf('\u00a7');
+                            if (lastColor >= max - 2) {
+                                suffix = CC.getLastColors(translated.substring(0, Math.min(translated.length(), max + 1)))
+                                        + translated.substring(lastColor + 2);
+                                prefix = prefix.substring(0, lastColor);
                             } else {
-                                prefix = translated.substring(0, max);
-                                int lastColor = prefix.lastIndexOf('\u00a7');
-                                if (lastColor >= max - 2) {
-                                    suffix = CC.getLastColors(translated.substring(0, Math.min(translated.length(), max + 1)))
-                                            + translated.substring(lastColor + 2);
-                                    prefix = prefix.substring(0, lastColor);
-                                } else {
-                                    suffix = CC.getLastColors(prefix) + translated.substring(max);
-                                }
-                                if (suffix.length() > max) {
-                                    suffix = suffix.substring(0, max);
-                                }
+                                suffix = CC.getLastColors(prefix) + translated.substring(max);
                             }
-                            if (!prefix.equals(boardEntry.team.getPrefix())) {
-                                boardEntry.team.setPrefix(prefix);
-                            }
-                            if (!suffix.equals(boardEntry.team.getSuffix())) {
-                                boardEntry.team.setSuffix(suffix);
+                            if (suffix.length() > max) {
+                                suffix = suffix.substring(0, max);
                             }
                         }
+                        if (!prefix.equals(boardEntry.team.getPrefix())) {
+                            boardEntry.team.setPrefix(prefix);
+                        }
+                        if (!suffix.equals(boardEntry.team.getSuffix())) {
+                            boardEntry.team.setSuffix(suffix);
+                        }
                     }
-                    i++;
                 }
-
-                if (!player.getScoreboard().equals(board.scoreboard)) {
-                    player.setScoreboard(board.scoreboard);
-                }
-            } catch (Exception ex) {
-                LibraryPlugin.getInstance().getPlugin().getLogger()
-                        .severe("Scoreboard error for " + player.getName() + ": " + ex.getMessage());
+                i++;
             }
-            return false;
-        });
+
+            if (!player.getScoreboard().equals(board.scoreboard)) {
+                player.setScoreboard(board.scoreboard);
+            }
+        } catch (Exception ex) {
+            LibraryPlugin.getInstance().getPlugin().getLogger()
+                    .severe("Scoreboard error for " + player.getName() + ": " + ex.getMessage());
+        }
     }
 
     public void createBoard(Player player) {
@@ -164,6 +167,8 @@ public final class BoardManager implements Runnable {
         }
         board.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
         boards.put(player.getUniqueId(), board);
+        // The board is empty and unassigned until it is filled, so do it here instead of waiting for the next update.
+        update(player, board);
     }
 
     public void removeBoard(Player player) {
